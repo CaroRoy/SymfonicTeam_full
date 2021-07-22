3c569acb8b22145f77d96c9cecd01b75741a021d
Caro <caroline.j.roy@gmail.com>
2021-07-22 13:46:39 +0200

    event list : knp paginator

--- src/Controller/Event/EventListController.php
+++ src/Controller/Event/EventListController.php
@@ -3,17 +3,21 @@
 namespace App\Controller\Event;
 
 use App\Repository\EventRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class EventListController extends AbstractController {
     /**
      * @Route("/seances", name="event_list")
      */
-    public function list(EventRepository $eventRepository): Response {
-        $events = $eventRepository->findAll();
+    public function list(EventRepository $eventRepository, PaginatorInterface $paginatorInterface, Request $request): Response {
+        $all = $eventRepository->findAll();
+        // on part à la page 1 par défaut, sinon le numéro de la page appelée, et on présente 5 events par page
+        $events = $paginatorInterface->paginate($all, $request->query->getInt('page', 1), 5);
 
-        return $this->render("event/event_list.html.twig",['events' => $events]);
+        return $this->render("event/event_list.html.twig",['all' => $all, 'events' => $events]);
     }
 }
