09af6e67428b604a2ce1b380dd032c958e124f41
Caro <caroline.j.roy@gmail.com>
2021-07-23 15:37:09 +0200

    verification age for registration

--- src/Controller/User/RegistrationController.php
+++ src/Controller/User/RegistrationController.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\MesServices\EmailService;
 use App\MesServices\ImageService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,7 +18,7 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/register", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, ImageService $imageService, EmailService $emailService): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, ImageService $imageService, EmailService $emailService, EntityManagerInterface $em): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -35,12 +36,24 @@ class RegistrationController extends AbstractController
             /** @var User $user */
             $user = $form->getData();
 
+            // Vérification âge :
+            $formBirthdate = $form->get('birthdate')->getData();
+            $birthdate = $formBirthdate->format('Y-m-d H:i:s');
+            $today = date("Y-m-d H:i:s");
+            $diff = date_diff(date_create($birthdate), date_create($today));
+            $age = $diff->format('%y');
+
+            if ($age < 18) {
+                $this->addFlash('danger','Désolé, tu n\'as pas encore 18 ans. Mais nous serons ravis de t\'accueillir sur Symfonic Team dans quelque temps !');
+                return $this->redirectToRoute('home');    
+            }
+
+            // récupération et enregistrement de l'avatar :
             $image = $form->get('avatar')->getData();
             $imageService->save($image,$user);
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $em->persist($user);
+            $em->flush();
             // do anything else you need here, like send an email
 
             $emailService->sendWelcome($user);
