4c85e72b07f73be150694c4b2acaa94149e20391
Caro <caroline.j.roy@gmail.com>
2021-09-10 18:17:28 +0200

    Commentaires sur filtre KNP

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -57,16 +57,19 @@ class EventRepository extends ServiceEntityRepository
     */
 
     /**
-     * on récupère les Events par une recherche
+     * Récupère les séances en fonction du filtre choisi
      * @return  PaginationInterface
      */
     public function findSearch(SearchData $searchData) : PaginationInterface {
+        // On crée une requête qui récupère toutes les séances et qui les trie par id décroissant
         $query = $this
             ->createQueryBuilder('e')
             ->select('e')
             ->orderBy('e.id', 'DESC')
         ;
 
+        // Si le champ de recherche q n'est pas vide,
+        // alors on filtre les séances pour avoir celles qui contiennent le mot-clé
         if (!empty($searchData->q)) {
             $query = $query
                 ->andWhere('e.title LIKE :q')
@@ -79,12 +82,16 @@ class EventRepository extends ServiceEntityRepository
                 ->setParameter('q', "%{$searchData->q}%");
         }
 
+        // Si le champ de recherche postalCode n'est pas vide,
+        // alors on filtre les séances pour avoir celles dont le code postal correspond
         if (!empty($searchData->postalCode)) {
             $query = $query
                 ->andWhere('e.meetingPostalCode LIKE :postalCode')
                 ->setParameter('postalCode', "%{$searchData->postalCode}%");
         }
 
+        // Si le champ de recherche dateMin n'est pas vide,
+        // alors on filtre les séances pour avoir celles dont la date est sup ou égale à dateMin
         if (!empty($searchData->dateMin)) {
             $query = $query
                 ->andWhere('e.meetingDatetime >= :dateMin')
@@ -92,6 +99,8 @@ class EventRepository extends ServiceEntityRepository
                 ->orderBy('e.meetingDatetime', 'DESC');
         }
 
+        // Si le champ de recherche dateMax n'est pas vide,
+        // alors on filtre les séances pour avoir celles dont la date est inf ou égale à dateMax
         if (!empty($searchData->dateMax)) {
             $date = $searchData->dateMax;
             $datetime = $date->format('Y-m-d H:i:s');
@@ -103,12 +112,16 @@ class EventRepository extends ServiceEntityRepository
                 ->orderBy('e.meetingDatetime', 'DESC');
         }
 
+        // Si le champ de recherche instrument n'est pas vide,
+        // alors on filtre les séances pour avoir celles dont l'instrument correspond
         if (!empty($searchData->instrument)) {
             $query = $query
                 ->andWhere('e.instrument LIKE :instrument')
                 ->setParameter('instrument', "%{$searchData->instrument}%");
         }
 
+        // Si le champ de recherche typeOfMusic n'est pas vide,
+        // alors on filtre les séances pour avoir celles dont le style de musique correspond
         if (!empty($searchData->typeOfMusic)) {
             $query = $query
                 ->andWhere('e.typeOfMusic LIKE :style')
@@ -116,7 +129,9 @@ class EventRepository extends ServiceEntityRepository
         }
 
         $query = $query->getQuery();
-        // on retourne les résultats dans une pagination en partant de la page appelé via SearchData(par défaut 1), et on met 5 résultats par page
+        // on retourne les résultats dans une pagination
+        // en partant de la page appelé via SearchData(par défaut 1)
+        // et on met 5 résultats par page
         return $this->paginator->paginate($query, $searchData->page, 5);
     }
 }
